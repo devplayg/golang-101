92196c589ffd4f727b5d075a6b18cf7956cfd218
wsan <devplayg@gmail.com>
2019-04-01 14:28:17 +0900

    added 102 server

---
+++ bytes-trans-102-client/main.go
@@ -0,0 +1,146 @@
+// blog: Marcio
+
+package main
+
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/binary"
+	"encoding/gob"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+const (
+	dataSize = 1024 * 1024
+)
+
+type Message struct {
+	Seq       int64
+	Timestamp int64
+	Data      []byte
+	Hash      []byte
+}
+
+func (m *Message) Merge() []byte {
+	return bytes.Join(
+		[][]byte{
+			IntToHex(m.Seq),
+			IntToHex(m.Timestamp),
+			m.Data,
+		},
+		[]byte(""),
+	)
+}
+
+func (m *Message) Serialize() ([]byte, error) {
+	var buf bytes.Buffer
+	encoder := gob.NewEncoder(&buf)
+	err := encoder.Encode(m)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func main() {
+
+	// Set flags
+	var (
+		cmdFlags = flag.NewFlagSet("", flag.ExitOnError)
+		host     = cmdFlags.String("host", "127.0.0.1", "Host")
+		port     = cmdFlags.String("port", "8000", "Port")
+	)
+	cmdFlags.Usage = func() {
+		cmdFlags.PrintDefaults()
+	}
+	cmdFlags.Parse(os.Args[1:])
+
+	// Connect to server
+	conn, err := net.Dial("tcp", *host+":"+*port)
+	if err != nil {
+		log.Println("failed to connect to server", err)
+		return
+	}
+	defer conn.Close()
+
+	go send(conn)
+
+	waitForSignals()
+}
+
+func send(conn net.Conn) {
+	var seq int64 = 1
+	for {
+		// Create message
+		m := Message{
+			Seq:       seq,
+			Timestamp: time.Now().Unix(),
+			Data:      getData(dataSize),
+		}
+		merged := m.Merge()
+		hash := sha256.Sum256(merged)
+		m.Hash = hash[:]
+
+		data, err := m.Serialize()
+		if err != nil {
+			log.Println("failed to serialize;", err)
+			continue
+		}
+		//spew.Dump(data)
+
+		// Send data size
+		dataSize := int64(len(data))
+		//log.Println(IntToHex(dataSize) )
+		_, err = conn.Write(IntToHex(dataSize))
+		if err != nil {
+			log.Println("failed to send data;", err)
+		}
+		//spew.Dump(dataSize)
+
+		// Send data
+		_, err = conn.Write(data)
+		if err != nil {
+			log.Println("failed to send;", err)
+		}
+
+		if seq%100 == 0 {
+			log.Printf("[%3d] len=%-4d", m.Seq, dataSize)
+		}
+
+		seq++
+		time.Sleep(10 * time.Millisecond)
+	}
+}
+
+func getData(size int) []byte {
+	data := make([]byte, size)
+	rand.Read(data)
+	return data
+}
+
+func waitForSignals() {
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
+	select {
+	case <-signalCh:
+		fmt.Print("Signal received, shutting down...")
+	}
+}
+
+func IntToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return buff.Bytes()
+}
---
+++ bytes-trans-102-server/main.go
@@ -0,0 +1,199 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"encoding/gob"
+	"flag"
+	"io"
+	log "github.com/sirupsen/logrus"
+	"net"
+	"os"
+)
+
+const (
+	BufferSize = 1024
+)
+
+// Message object
+type Message struct {
+	Seq       int64
+	Timestamp int64
+	Data      []byte
+	Hash      []byte
+}
+
+// Merge data
+func (m *Message) Merge() []byte {
+	seq := IntToHex(m.Seq)
+	ts := IntToHex(m.Timestamp)
+	merged := bytes.Join(
+		[][]byte{
+			seq,
+			ts,
+			m.Data,
+		},
+		[]byte(""),
+	)
+
+	return merged
+}
+
+// Verify data
+func (m *Message) Verify() bool {
+	data := m.Merge()
+	hash := sha256.Sum256(data)
+	if bytes.Equal(m.Hash, hash[:]) {
+		return true
+	}
+	return false
+}
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{
+		DisableColors: true,
+		FullTimestamp: true,
+	})
+}
+
+func main() {
+	var (
+		cmdFlags = flag.NewFlagSet("", flag.ExitOnError)
+		host     = cmdFlags.String("host", "127.0.0.1", "receieve host")
+		port     = cmdFlags.String("port", "8000", "receieve port")
+		debug    = cmdFlags.Bool("debug", false, "debug")
+	)
+	cmdFlags.Usage = func() {
+		cmdFlags.PrintDefaults()
+	}
+	cmdFlags.Parse(os.Args[1:])
+
+	// Debug
+	if *debug {
+		log.SetLevel(log.DebugLevel)
+		log.Debug("start debugging")
+	}
+
+	// Start receiver
+	ln, err := net.Listen("tcp", *host+":"+*port)
+	if nil != err {
+		log.Panic(err)
+	}
+	defer ln.Close()
+	log.Infof("receiver started listening on %s:%s", *host, *port)
+
+	// Accept connections
+	closedConn := make(chan net.Conn, 4)
+	go func(ch chan net.Conn) {
+		for {
+			conn, err := ln.Accept()
+			if nil != err {
+				log.Error("failed to accept;", err)
+				continue
+			}
+			go handleConnection(conn, ch)
+		}
+	}(closedConn)
+
+
+	for {
+		select {
+		case conn:= <-closedConn :
+			err := conn.Close()
+			if err != nil {
+				log.Errorf("Closing connection: %s", err)
+			}
+		}
+	}
+}
+
+func handleConnection(conn net.Conn, closeConn chan<- net.Conn) {
+	log.Infof("new connection %v", conn.RemoteAddr().String())
+	sizeBuf := make([]byte, 8)
+
+	buf := make([]byte, BufferSize)
+
+	for {
+		// Read data size
+		n, err := conn.Read(sizeBuf)
+		if nil != err {
+			if io.EOF == err {
+				log.Errorf("closed from client; %v", conn.RemoteAddr().String())
+				closeConn <- conn
+				return
+			}
+			log.Errorf("fail to receive data; err: %v", err)
+			closeConn <- conn
+			return
+		}
+		if 0 < n {
+			dataSize := int64(binary.BigEndian.Uint64(sizeBuf[:n]))
+
+			var read int64
+			data := make([]byte, 0)
+			for read < dataSize {
+				n, err := conn.Read(buf)
+				if err != nil {
+					if io.EOF == err {
+						log.Errorf("closed from client; %v", conn.RemoteAddr().String())
+						closeConn <- conn
+						return
+					}
+					log.Error("failed to read;", err)
+					closeConn <- conn
+					return
+				}
+				if (dataSize - read) < BufferSize {
+					last := dataSize - read
+					read += last
+					data = append(data, buf[:last]...)
+
+				} else {
+					read += int64(n)
+					data = append(data, buf[:n]...)
+
+				}
+				log.Debugf("total=%d, read=%d, merged=%d, \n", dataSize, n, read)
+			}
+			m, err := Deserialize(data)
+			if err != nil {
+				log.Error("failed to deserialize;", err)
+				continue
+			}
+			if m.Verify() {
+				log.Debugf("==> seq=%d, timestamp=%d, equal=%v", m.Seq, m.Timestamp, true)
+			} else {
+				log.Infof("==> seq=%d, timestamp=%d, equal=%v", m.Seq, m.Timestamp, false)
+			}
+
+			if m.Seq%100 == 0 {
+				log.Infof("==> seq=%d, timestamp=%d, equal=%v", m.Seq, m.Timestamp, true)
+			}
+
+		}
+	}
+}
+
+func IntToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil {
+		log.Error("failed to convert int to hex;", num)
+		return nil
+	}
+
+	return buff.Bytes()
+}
+
+func Deserialize(b []byte) (*Message, error) {
+	var m Message
+
+	decoder := gob.NewDecoder(bytes.NewReader(b))
+	err := decoder.Decode(&m)
+	if err != nil {
+		return nil, err
+	}
+
+	return &m, nil
+}
